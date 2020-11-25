a0c5afb8288c40c3b938a80a479124bee1a5ac36
chipfody <60615532+chipfody@users.noreply.github.com>
2020-11-25 12:08:34 -0500

    worked a few more questions

--- src/HomePage.java
+++ src/HomePage.java
@@ -45,25 +45,39 @@ public class HomePage {
 //        }
 
 
-                int[][] list = {{1,13,5},{1,2,5},{2,7,2}};
-                int searchValue = 2;
-                int positionX = -1;
-                int positionY = -1;
-                PARENT_LOOP: for(int i=0; i<list.length; i++) {
-                    for(int j=0; j<list[i].length; j++) {
-                        if(list[i][j]==searchValue) {
-                            positionX = i;
-                            positionY = j;
-                            break PARENT_LOOP;
-                        }
-                    }
-                }
-                if(positionX==-1 || positionY==-1) {
-                    System.out.println("Value "+searchValue+" not found");
-                } else {
-                    System.out.println("Value "+searchValue+" found at: " +
-                            "("+positionX+","+positionY+")");
-                }
+//                int[][] list = {{1,13,5},{1,2,5},{2,7,2}};
+//                int searchValue = 2;
+//                int positionX = -1;
+//                int positionY = -1;
+//                PARENT_LOOP: for(int i=0; i<list.length; i++) {
+//                    for(int j=0; j<list[i].length; j++) {
+//                        if(list[i][j]==searchValue) {
+//                            positionX = i;
+//                            positionY = j;
+//                            break PARENT_LOOP;
+//                        }
+//                    }
+//                }
+//                if(positionX==-1 || positionY==-1) {
+//                    System.out.println("Value "+searchValue+" not found");
+//                } else {
+//                    System.out.println("Value "+searchValue+" found at: " +
+//                            "("+positionX+","+positionY+")");
+//                }
+
+//        for(int i=0; i<10 ; ) {
+//            i = i++;
+//            System.out.println("Hello World");
+//            }
+
+        byte m = 40, n = 50;
+        byte sum = (byte) (m + n);
+        System.out.println(sum);
+
+        int x1 = 50, x2 = 75;
+        boolean p = x1 >= x2;
+        if(p = true) System.out.println("Success");
+         else System.out.println("Failure");
 
 
     } //main
