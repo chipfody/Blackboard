52f07b6d126e4d5c7abf3b988568b9f4e1986064
chipfody <60615532+chipfody@users.noreply.github.com>
2020-12-21 17:29:45 -0500

    certification operands

---
+++ src/operators/Operands.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class Operands {
+
+    public static void main(String[] args) {
+        char char1 = 'a';
+        System.out.println(char1);
+        System.out.println(char1 + char1);
+        System.out.println(char1 - char1);
+
+        final byte age1 = 10;
+        final byte age2 = 5;
+        short sum = age1 + age2;
+        System.out.println("\n" + sum);
+    }
+}
