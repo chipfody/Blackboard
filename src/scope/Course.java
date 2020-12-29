4e022a893b79a17f6765c3375ff336a7591efe8f
chipfody <60615532+chipfody@users.noreply.github.com>
2020-12-29 14:42:54 -0500

    cert - course check

---
+++ src/scope/Course.java
@@ -0,0 +1,25 @@
+package scope;
+
+public class Course {
+    void enroll (long duration) {
+        System.out.println("long");
+    }
+    void enroll (int duration) {
+        System.out.println("int");
+    }
+    void enroll (String s) {
+        System.out.println("String");
+    }
+    void enroll (Object o) {
+        System.out.println("Object");
+    }
+}
+
+class EJavaGuru{
+    public static void main(String[] args) {
+        Course course = new Course();
+        char c = 10;
+        course.enroll(c);
+        course.enroll("Object");
+    }
+}
--- src/scope/TestPhone.java
+++ src/scope/TestPhone.java
@@ -2,9 +2,10 @@ package scope;
 
 public class TestPhone {
     public static void main(String[] args) {
-    Phone p1 = new Phone();
-    p1.setNumber();
-        System.out.println(p1.phoneNumber);
+//    Phone p1 = new Phone();
+//    p1.setNumber();
+//        System.out.println(p1.phoneNumber);
+
         
     }
 }
