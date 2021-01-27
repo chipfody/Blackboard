1f2e56b8f5b7b83f0ef06eea9c8bf1017f1ae0a0
chipfody <60615532+chipfody@users.noreply.github.com>
2021-01-27 16:17:20 -0500

    cert - review

--- src/Chapter4/Primate.java
+++ src/Chapter4/Primate.java
@@ -40,4 +40,90 @@ class Fish extends Bird {
     }
 }
 
+class Primate2 {
+    public Primate2() {
+        System.out.println("Primate");
+    }
+}
+class Ape extends Primate2 {
+    public Ape() {
+        System.out.println("Ape");
+    }
+}
+class Chimpanzee extends Ape {
+    public static void main(String[] args) {
+        new Chimpanzee();
+    }
+}
+
+class Fish2 {
+    protected int size;
+    private int age;
+    public Fish2(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+}
+class Shark extends Fish2 {
+    private int numberOfFins = 8;
+    public Shark(int age) {
+        super(age);
+        this.size = 4;
+    }
+    public void displaySharkDetails() {
+        System.out.print("Shark with age: "+getAge());
+        System.out.print(" and "+super.size+" meters long");
+        System.out.print(" with "+numberOfFins+" fins");
+    }
+
+    public static void main(String[] args) {
+        Shark shark = new Shark(3);
+        shark.displaySharkDetails();
+    }
+}
+
+class Canine {
+    String color = "Brown";
+    public double getAverageWeight() {
+        return 50;
+    }
+}
+class Wolf extends Canine {
+    String color = "Gray";
+    public double getAverageWeight() {
+        return super.getAverageWeight()+20;
+    }
+    public static void main(String[] args) {
+        Canine canine = new Wolf();
+        System.out.println(new Canine().getAverageWeight());  // 50.0
+        System.out.println(new Wolf().getAverageWeight());  // 70.0
+        System.out.println(canine.color);  //Brown
+        System.out.println(((Wolf)canine).color);  //Gray
+    }
+}
+
+class Marsupial {
+    public static boolean isBiped() {
+        return false;
+    }
+    public void getMarsupialDescription() {
+        System.out.println("Marsupial walks on two legs: "+isBiped());
+    }
+}
+class Kangaroo extends Marsupial {
+    public static boolean isBiped() {
+        return true;
+    }
+    public void getKangarooDescription() {
+        System.out.println("Kangaroo hops on two legs: "+isBiped());
+    }
+    public static void main(String[] args) {
+        Kangaroo joey = new Kangaroo();
+        joey.getMarsupialDescription();
+        joey.getKangarooDescription();
+    }
+}
+
 
