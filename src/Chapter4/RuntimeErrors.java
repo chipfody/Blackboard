3d3a6940fb5349f67e1883043ffe4a6bb50a344d
chipfody <60615532+chipfody@users.noreply.github.com>
2021-02-02 17:47:36 -0500

    cert - review continued

--- src/Chapter4/RuntimeErrors.java
+++ src/Chapter4/RuntimeErrors.java
@@ -242,3 +242,25 @@ class Laptop {
         new Laptop().start();
     }
 }
+
+class YetAnotherExample {
+    public static void main(String[] args) {
+        System.out.print("a");
+        try {
+            System.out.print("b");
+            throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            System.out.print("c");
+            throw new RuntimeException("1");
+        } catch (RuntimeException e) {
+            System.out.print("d");
+            throw new RuntimeException("2");
+        } finally {
+            System.out.print("e");
+            throw new RuntimeException("3");
+        }
+    }
+}
+
+
+
